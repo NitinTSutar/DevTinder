ea445b4e004ba696d261809f3a0ac69c65d917c2
NitinTSutar <nitintsuthar67@gmail.com>
2025-07-28 00:57:14 +0530

    added navBar and footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Login from "./pages/Login.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/login" element={<Login/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
